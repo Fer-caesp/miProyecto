051a19d3a65a0405ff629935dfae3cc35b8f5a51
Luis Carrillo <luis.carrilloesp@gmail.com>
2022-11-08 18:43:36 -0600

    Segundo Comit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,14 @@
 import './estilosNavbar.css'
+
 export function Navbar({nombre, ciudad, direccion}){
     var estilos = {background:'#000', padding: '20px', color:'#fff'}
     
     return <>
         <ul>
             <li style={estilos}>
+                Hola {nombre} de {ciudad} en la colionia {direccion.colonia?direccion.colonia:"N/A"}, en la casa con el número #{direccion.noCasa?direccion.noCasa:"N/A"}
+            </li>
+            <li className='estilasho'>
                 Hola {nombre} de {ciudad} en la colionia {direccion.colonia?direccion.colonia:"N/A"}, en la casa con el número #{direccion.noCasa?direccion.noCasa:"N/A"}
             </li>
             <li className='estilasho'>
